b6ab25a7b9dab3c24b117f03db594f156a5dfe4d
caomingkai <caomingkai@outlook.com>
2017-08-06 01:06:28 -0700

    8th edition

--- BundleCheckInject.php
+++ BundleCheckInject.php
@@ -6,25 +6,76 @@
     PHPShopify\ShopifySDK::config($config);
     $shopify = new PHPShopify\ShopifySDK;
 
-    echo '<h1>$shopify below : </h1>' .  "\n";
-    echo "<pre>";
-    $themeID = $shopify->Theme->get()[0]['id'];
-    print_r($shopify->Theme($themeID)->Asset->get());
-    echo "</pre>";
-    echo '<p> ------------------------  </p>' .  "\n";
+//-----## 0 ## find out the main Theme.liquid, and its ID
+    $themes = $shopify->Theme->get();
+    $numsOfThemes = count($theme);
+    $themeID = 0;
+    foreach( $themes as $oneTheme ){
+      if($oneTheme['role'] === 'main'){
+        $themeID = $oneTheme['id'];
+        break;
+      }
+    }
 
-    // ##### 0 ##### see if can inject seccessfully
-    // 1. find out the cart.liquid which contain the for-loop
-    // 1.1 could firstly find out few possible fileName, and then check their content, if some one have keywords, then that's the file. set flag
-    // 1.2 if those possible file still don't have keywords, set flag false.
-    // 1.3 next we keep looping through templete/snippet/section to find file bearing name with 'cart', then look in content to find keywords
+//-----## 1 ## insert bundleCheck.liquid to shopify admin Snippet
+    // the code snippet to be inserted
+    $codeInject = '<script>
+                        {% assign flag = false %}
+                        {% for item in cart.items %}
+                                  {% if item.variant.id == 37696175173 %}
+                                {% assign flag = true %}
+                                  {% endif %}
+                        {% endfor %}
 
-    // 2. create bundleCheck.liquid
+                        {% if flag==true %}
+                              document.getElementById("test").innerHTML = "has product: a,1";
+                        {% else %}
+                              document.getElementById("test").innerHTML = "dont hava product: a,1";
+                        {% endif %}
+                          function addToCart() {
+                              var xhttp = new XMLHttpRequest();
+                              xhttp.onreadystatechange = function() {
+                                if (this.readyState == 4 && this.status == 200) {
+                                  document.getElementById("test").innerHTML =
+                                    "ADD e SUCCEED!";
+                                }
+                              };
+                              xhttp.open("POST", "/cart/add", true);
+                              xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                              xhttp.send("id=37144461061&add=");
+                          }
 
-    // 3. append {{ include bundleCheck.liquid }} this sentence at the end of target cart.liquid file.
+                  </script>';
+    $para = array(
+      "key" => "snippets/bundleCheck.liquid",
+      "value" => $codeInject
+    );
+    $bundleCheckSnippet = $shopify->Theme($themeID)->Asset->put($para) ;
 
-    // ##### 1 ##### directly write a liquid file, and put it in shopAdmin, implement those functions. see if can function well.
 
-    // 4. convert bundleCheck.liquid into php file, do the appending to cart.liquid
+
+//-----## 2 ## insert the statement: {% include 'bundleCheck' %} to 'theme.liquid' file
+    // find out theme.liquid, based on themeID already got
+    $para = array(
+      "asset[key]" => "layout/theme.liquid",     // Note: the key is 'asset[key]', NOT 'key' !
+    );
+    $themeContent = $shopify->Theme($themeID)->Asset->get($para)['asset']['value'] ;
+    // find out <head> tag, in order to insert into this statement: "{% include 'bundleCheck' %}"
+    $posOfHead = strpos( $themeContent, '<head>');
+    $statementInject = "\n" . "  {% include 'bundleCheck' %}";
+    $themeContentNew = substr_replace( $themeContent , $statementInject,  $posOfHead+6, 0 );
+    // insert statement  "{% include 'bundleCheck' %}", right after <head> tag
+    $para = array(
+      "key" => "layout/theme.liquid",
+      "value" => $themeContentNew
+    );
+    $themeContentNew = $shopify->Theme($themeID)->Asset->put($para) ;
+
+                                                echo '<h1>$shopify below : </h1>' .  "\n";
+                                                echo "<pre>";
+                                                print_r ($themeContentNew);
+                                                echo "</pre>";
+                                                echo '<p> ------------------------  </p>' .  "\n";
+
 
 ?>
